d30db7d7ae91c46d4b93f1fe1fcf5a52aa52637b
yidahc <46684324+yidahc@users.noreply.github.com>
2019-11-29 20:47:16 -0600

    added express and a simple get and post endpoint

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
-const request = require ('request');
+express = require ('express')
 
-POKEURL= 'https://pokeapi.co/api/v2';
-SWURL= 'https://swapi.co/api';
+app = express();
 
-const getPokeById = id => {
-    request.get(`${POKEURL}/pokemon/${id}/`, (error, response, body) => {
-        const Pokemon = JSON.parse(body);
-        console.log(`My name is ${Pokemon.name} and these are all my moves:`)
-        
-      //  for (let key in Pokemon.moves) {
-      //      console.log (Pokemon.moves[key].move.name);
-      //  }
-        
-        const map1 = Pokemon.moves.map(x => x.move.name);
-         console.log(map1);
-         
-        // console.log(Pokemon.moves);
-        // console.log(response.statusCode)
-       //  console.log(response)  <-- this would also include the headers and other information the server is sending back, 
-        // console.log(response.body) this is why we always use response.body
-    })
-};
+const port = 3000;
 
+app.get('/', (req, res) => res.send('<h1>Hello Thor</h1>'))
 
-const getPeopleMovies = (id => {
-    request.get(`${SWURL}/people/${id}/`, (error, response, body) => {
-        const person = JSON.parse(body);
-        console.log(`My name is ${person.name} and these are the movies I appear in`);
-            person.films.map (film => {
-                request.get(film, (error, response, body) => {
-                    const film = JSON.parse(body);
-                    console.log(film.title);  
-                });
-            });
-    });
-});
+app.post('/register', (req, res) => {
+    console.log(request);
+    res.json({user:{
+        user,
+    }})
+})
 
-getPokeById(7);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-getPeopleMovies(13);
-
-
+// http is the protocol that we use for requests, which involves get/post, etc
+// 200 was succesfully retrieved or found 
+// 201 was succesfully created (for a succesful post)
+// 400 bad request
+// 401 unauthorized (must be authenticated. has not logged on)
+// 404 not found
+// 405 method not allowed (the methid being used did exist but has now been disabled)
+// 403 forbidden (can not be authorized. you are not an authorized user or you have not paid. or your IP is not whitelisted)
+// 500 internal server error
+// 502 bad gateway (headers or tokens needed)
+// 503 server error (the server does not know what to do with this error, but it does not have to be an issue with your path or request)
+// 505 http version not supported
