f95b61551e517c5ae43453fd927b19f80cc2d69f
yidahc <46684324+yidahc@users.noreply.github.com>
2019-11-29 21:55:00 -0600

    removing node modules

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 express = require ('express')
 
 app = express();
-
-const port = 3000;
+app.use(express.json()); // middlewear converting the requests into something we can now access without needing body-parser
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('<h1>Hello Thor</h1>'))
 
+
 app.post('/register', (req, res) => {
-    console.log(request);
-    res.json({user:{
+    console.log(req.body); // we access the body of the request
+    user = req.body.user;
+    res.json({user:{ // params accesses the parameters in the url request (after the : in a get request url)
         user,
     }})
+})  
+
+app.get('user/:id', (req, res) => {
+  console.log (req.params.id);
+  res.status(200).json({id: req.params.id})  
 })
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
